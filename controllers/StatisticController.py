a2d3c50754d5e0c789babb35d4d4a67f45007f6c
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-03-29 13:28:04 -0300

    Construindo o controller para a rota estatistica

---
+++ controllers/StatisticController.py
@@ -0,0 +1,40 @@
+from validators.date_validator import last_seconds
+from flask import jsonify
+
+
+class StatisticController:
+
+    def get_statistics(self, transactions:list):
+        try:
+            valid_values = []
+            
+            for transaction in transactions:
+                
+                key = next(iter(transaction))
+                
+                if last_seconds(key):
+                    valid_values.append(transaction[key]["valor"])
+            
+            if len(valid_values) == 0:
+                return jsonify(
+                    {
+                        "count" : 0,
+                        "sum" : 0,
+                        "avg" : 0,
+                        "min" : 0,
+                        "max" : 0
+                    }
+                ), 200
+            
+            return jsonify(
+                {
+                    "count" : len(valid_values),
+                    "sum" : sum(valid_values),
+                    "avg" : sum(valid_values)/len(valid_values),
+                    "min" : min(valid_values),
+                    "max" : max(valid_values)
+                }
+            ), 200
+        
+        except:
+            return "", 500
