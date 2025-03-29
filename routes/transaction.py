f9f8c544d5aa56a8df65626061cb1782d51c69d2
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-03-29 11:19:46 -0300

    Construindo o DELETE da rota /transacao

--- routes/transaction.py
+++ routes/transaction.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
+from controllers.TransactionController import TransactionController
 
 transaction_route = Blueprint("transaction_route", __name__, url_prefix="/")
 
+transactions = TransactionController()
+
 @transaction_route.route("/transacao", methods=["POST"])
 def insert_transaction():
-    if request.method == "GET":
-        pass
+    if request.method == "POST":
+        print(transactions.statistics)
+        transactions.insert(request)
+        
     else:
-        return 405
+        return Response(status=405)
+
+@transaction_route.route("/transacao", methods=["DELETE"])
+def delete_all_transaction():
+    if request.method == "DELETE":
+        print(transactions.statistics)
+        transactions.delete()
+    else:
+        return Response(status=405)
