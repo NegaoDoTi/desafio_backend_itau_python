701b6439fdd876e42856f44432f6775e17f268ff
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-03-29 11:54:22 -0300

    Mudança do atributo statistics para transactions para melhor entendimento

--- controllers/TransactionController.py
+++ controllers/TransactionController.py
@@ -1,35 +1,34 @@
 from validators.date_validator import is_iso8601, is_past
-from flask import Response
 
 class TransactionController:
     
     def __init__(self):
-        self.statistics:list = []
+        self.transactions:list = []
     
     def insert(self, req):
         try:
             data = req.get_json()
             
             if "valor" not in data or "dataHora" not in data:
-                return 400
+                return "", 400
             
             if not isinstance(data["valor"], float) or data["valor"] < 0:
-                return 422
+                return "", 422
             
-            if not is_iso8601(data["dataHora"]) or not is_past("dataHora"):
-                return 422
+            if not is_iso8601(data["dataHora"]) or not is_past(data["dataHora"]):
+                return "", 422
             
             self.statistics.append(data)
             
-            return 201
+            return "", 201
         except:
-            return 500
+            return "", 500
     
     def delete(self):
         try:
-            self.statistics = []
+            self.transactions = []
             
-            200
+            return "", 200
         
         except:
-            return 500
+            return "", 500
--- routes/transaction.py
+++ routes/transaction.py
@@ -3,16 +3,16 @@ from controllers.TransactionController import TransactionController
 
 transaction_route = Blueprint("transaction_route", __name__, url_prefix="/")
 
-transactions = TransactionController()
+transaction_controller = TransactionController()
 
 @transaction_route.route("/transacao", methods=["POST"])
 def insert_transaction():
     if request.method == "POST":
-        print(transactions.statistics)
-        return transactions.insert(request)
+        print(transaction_controller.transactions)
+        return transaction_controller.insert(request)
 
 @transaction_route.route("/transacao", methods=["DELETE"])
 def delete_all_transaction():
     if request.method == "DELETE":
-        print(transactions.statistics)
-        return transactions.delete()
+        print(transaction_controller.transactions)
+        return transaction_controller.delete()
